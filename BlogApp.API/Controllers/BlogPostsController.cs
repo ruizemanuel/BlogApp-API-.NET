6a3f67c4b5d0498304551f91f32965d161a11a76
Emanuel Ruiz <ruiz.e.642@gmail.com>
2024-01-12 13:18:17 -0300

    added logic for post blogs

---
+++ BlogApp.API/Controllers/BlogPostsController.cs
@@ -0,0 +1,55 @@
+﻿using BlogApp.API.Models.Domain;
+using BlogApp.API.Models.DTO;
+using BlogApp.API.Repositories.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogPostsController : ControllerBase
+    {
+        private readonly IBlogPostRepository blogPostRepository;
+
+        public BlogPostsController(IBlogPostRepository blogPostRepository) 
+        {
+            this.blogPostRepository = blogPostRepository;
+        }
+        [HttpPost]
+
+        public async Task<IActionResult> CreateBlogPost([FromBody]CreateBlogPostRequestDto request)
+        {
+            //Dto to domain
+            var blogPost = new BlogPost
+            {
+                Author = request.Author,
+                Content = request.Content,
+                FeaturedImageUrl = request.FeaturedImageUrl,
+                IsVisible = request.IsVisible,
+                PublishDate = request.PublishDate,
+                ShortDescription = request.ShortDescription,
+                Title = request.Title,
+                UrlHandle = request.UrlHandle,
+            };
+
+            blogPost = await blogPostRepository.CreateAsync(blogPost);
+
+            //Domain to DTO
+            var response = new BlogPostDto
+            {
+                Id = blogPost.Id,
+                Author = blogPost.Author,
+                Content = blogPost.Content,
+                FeaturedImageUrl = blogPost.FeaturedImageUrl,
+                IsVisible = blogPost.IsVisible,
+                PublishDate = blogPost.PublishDate,
+                ShortDescription = blogPost.ShortDescription,
+                Title = blogPost.Title,
+                UrlHandle = blogPost.UrlHandle,
+            };
+
+            return Ok(response);
+        }
+    }
+}
---
+++ BlogApp.API/Repositories/Implementation/BlogPostRepository.cs
@@ -0,0 +1,22 @@
+﻿using BlogApp.API.Data;
+using BlogApp.API.Models.Domain;
+using BlogApp.API.Repositories.Interface;
+
+namespace BlogApp.API.Repositories.Implementation
+{
+    public class BlogPostRepository : IBlogPostRepository
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public BlogPostRepository(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public async Task<BlogPost> CreateAsync(BlogPost blogPost)
+        {
+            await dbContext.BlogPosts.AddAsync(blogPost);
+            await dbContext.SaveChangesAsync();
+            return blogPost;
+        }
+    }
+}
