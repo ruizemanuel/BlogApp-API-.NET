cd92071213fd8f9ad994644c85a009145eb65ecd
Emanuel Ruiz <ruiz.e.642@gmail.com>
2024-01-12 11:55:59 -0300

    Agregar archivos de proyecto.

---
+++ BlogApp.API/Repositories/Implementation/CategoryRepository.cs
@@ -0,0 +1,61 @@
+﻿using BlogApp.API.Data;
+using BlogApp.API.Models.Domain;
+using BlogApp.API.Repositories.Interface;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogApp.API.Repositories.Implementation
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public CategoryRepository(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public async Task<Category> CreateAsync(Category category)
+        {
+            await dbContext.Categories.AddAsync(category);
+            await dbContext.SaveChangesAsync();
+
+            return category;
+        }
+
+        public async Task<IEnumerable<Category>> GetAllAsync()
+        {
+           return await dbContext.Categories.ToListAsync();
+        }
+
+        public async Task<Category?> GetById(Guid id)
+        {
+           return await dbContext.Categories.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<Category?> UpdateAsync(Category category)
+        {
+            var existingCategory = await dbContext.Categories.FirstOrDefaultAsync(x => x.Id == category.Id);
+
+            if(existingCategory != null)
+            {
+                dbContext.Entry(existingCategory).CurrentValues.SetValues(category);
+                await dbContext.SaveChangesAsync();
+                return category;
+            }
+
+            return null;
+        }
+
+        public async Task<Category?> DeleteAsync(Guid id)
+        {
+            var existingCategory = await dbContext.Categories.FirstOrDefaultAsync(x => x.Id == id);
+            if(existingCategory != null)
+            {
+                dbContext.Categories.Remove(existingCategory);
+                await dbContext.SaveChangesAsync();
+                return existingCategory;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ BlogApp.API/Repositories/Interface/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using BlogApp.API.Models.Domain;
+
+namespace BlogApp.API.Repositories.Interface
+{
+    public interface ICategoryRepository
+    {
+        Task<Category> CreateAsync(Category category);
+        Task<IEnumerable<Category>> GetAllAsync();
+        Task<Category?> GetById(Guid id);
+        Task<Category?> UpdateAsync(Category category);
+        Task<Category?> DeleteAsync(Guid id);
+    }
+}
