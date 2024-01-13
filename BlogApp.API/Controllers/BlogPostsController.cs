9925899a997bc6ac6f80eff0ae6c617f301bd0c8
Emanuel Ruiz <ruiz.e.642@gmail.com>
2024-01-12 21:29:09 -0300

    added logic to link categories when creating blog posts

--- BlogApp.API/Controllers/BlogPostsController.cs
+++ BlogApp.API/Controllers/BlogPostsController.cs
@@ -11,10 +11,13 @@ namespace BlogApp.API.Controllers
     public class BlogPostsController : ControllerBase
     {
         private readonly IBlogPostRepository blogPostRepository;
+        private readonly ICategoryRepository categoryRepository;
 
-        public BlogPostsController(IBlogPostRepository blogPostRepository) 
+        public BlogPostsController(IBlogPostRepository blogPostRepository,
+            ICategoryRepository categoryRepository) 
         {
             this.blogPostRepository = blogPostRepository;
+            this.categoryRepository = categoryRepository;
         }
         [HttpPost]
 
@@ -31,7 +34,17 @@ namespace BlogApp.API.Controllers
                 ShortDescription = request.ShortDescription,
                 Title = request.Title,
                 UrlHandle = request.UrlHandle,
+                Categories = new List<Category>()
             };
+
+            foreach (var categoryGuid in request.Categories)
+            {
+                var existingCategory = await categoryRepository.GetById(categoryGuid);
+                if(existingCategory != null)
+                {
+                    blogPost.Categories.Add(existingCategory);
+                }
+            }
 
             blogPost = await blogPostRepository.CreateAsync(blogPost);
 
@@ -47,6 +60,13 @@ namespace BlogApp.API.Controllers
                 ShortDescription = blogPost.ShortDescription,
                 Title = blogPost.Title,
                 UrlHandle = blogPost.UrlHandle,
+                Categories = blogPost.Categories.Select(x => new CategoryDto 
+                { 
+                    Id = x.Id,
+                    Name = x.Name,
+                    UrlHandle = x.UrlHandle,
+                
+                }).ToList(),
             };
 
             return Ok(response);
