90feaf74bf0f18c4f501e1602e2334a73431692c
Emanuel Ruiz <ruiz.e.642@gmail.com>
2024-01-14 01:24:12 -0300

    imageController init and added method for validate image extension

---
+++ BlogApp.API/Controllers/ImagesController.cs
@@ -0,0 +1,39 @@
+﻿using BlogApp.API.Models.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> UploadImage([FromForm] IFormFile file, [FromForm] string fileName, [FromForm] string title)
+        {
+            ValidateFileUpload(file);
+            if(ModelState.IsValid)
+            {
+                var blogImage = new BlogImage
+                {
+                    FileExtension = Path.GetExtension(file.FileName).ToLower(),
+                    FileName = fileName,
+                    Title = title,
+                    DateCreated = DateTime.Now,
+                };
+            }
+        }
+        private void ValidateFileUpload(IFormFile file)
+        {
+            var allowedExtensions = new String[] { ".jpg", ".jpeg", ".png" };
+            if(!allowedExtensions.Contains(Path.GetExtension(file.FileName).ToLower()))
+            {
+                ModelState.AddModelError("file", "Unsupported file format");
+            }
+            if(file.Length > 10485760) 
+            {
+                ModelState.AddModelError("file", "File size cannot be more than 10MB");
+            }
+        }
+    }
+}
