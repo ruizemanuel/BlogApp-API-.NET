e7eab68ba5d10afe304f92981de4a7f032b73349
Emanuel Ruiz <ruiz.e.642@gmail.com>
2024-01-14 13:39:01 -0300

    serve static files from api

--- BlogApp.API/Program.cs
+++ BlogApp.API/Program.cs
@@ -2,6 +2,7 @@ using BlogApp.API.Data;
 using BlogApp.API.Repositories.Implementation;
 using BlogApp.API.Repositories.Interface;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -41,6 +42,12 @@ app.UseCors(options =>
 });
 
 app.UseAuthorization();
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "Images")),
+    RequestPath = "/Images"
+});
 
 app.MapControllers();
 
