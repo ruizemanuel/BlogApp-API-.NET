8b4c7bf2759d77d7ef0a2d67be1f9dcec1e8cfab
Emanuel Ruiz <ruiz.e.642@gmail.com>
2024-01-15 21:09:40 -0300

    added login method

--- BlogApp.API/Controllers/AuthController.cs
+++ BlogApp.API/Controllers/AuthController.cs
@@ -15,6 +15,37 @@ namespace BlogApp.API.Controllers
         {
             this.userManager = userManager;
         }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequestDto request)
+        {
+            var identityUser = await userManager.FindByEmailAsync(request.Email);
+
+            if(identityUser is not null)
+            {
+                //check password
+                var checkPasswordResult = await userManager.CheckPasswordAsync(identityUser, request.Password);
+
+                if(checkPasswordResult)
+                {
+                    var roles = await userManager.GetRolesAsync(identityUser);
+
+                    //create token and response
+                    var response = new LoginResponseDto()
+                    {
+                        Email = request.Email,
+                        Roles = roles.ToList(),
+                        Token = "TOKEN"
+                    };
+                    return Ok(response);
+                }
+            }
+            ModelState.AddModelError("", "Email or Password Incorrect");
+
+            return ValidationProblem(ModelState);
+        }
+
         [HttpPost]
         [Route("register")]
         public async Task<IActionResult> Register([FromBody] RegisterRequestDto request)
